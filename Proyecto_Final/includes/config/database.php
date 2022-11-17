e69a36bcda6a21c90bc04db58417067fe5ae4862
Luis Avila <lavila20658@ufide.ac.r>
2022-11-17 17:56:51 -0600

    Actualizando Login BD

--- Proyecto_Final/includes/config/database.php
+++ Proyecto_Final/includes/config/database.php
@@ -1,7 +1,7 @@
 <?php 
 
 function conectarDB() : mysqli {
-    $db = mysqli_connect('localhost', 'root', '', 'DBCasaCanguro');
+    $db = mysqli_connect('localhost', 'root', '', 'Canguro_DB');
 
     if(!$db) {
         echo "Error no se pudo conectar";
